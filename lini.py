7bdd3a18fe6fc5755f161ecfd8c0bf6ac4cf005d
Prarthana Gowda <p6361874024@gmail.com>
2026-01-22 11:49:17 +0530

    commit change

---
+++ lini.py
@@ -0,0 +1,11 @@
+n=int(input("enter number of elements:"))
+arr=list(map(int,input("enter elements:").split()))
+key=int(input("enter element to search:"))
+found=False
+for i in range(n):
+    if arr[i]==key:
+        print("elements found at postion",i+1)
+        found=True
+        break
+if not found:
+    print("element not found")    
